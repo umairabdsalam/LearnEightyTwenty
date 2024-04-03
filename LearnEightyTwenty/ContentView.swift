9ca973c2f0e83be2f2828983a723ece6b2afef77
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-04-03 16:55:33 +0100

    Initial Commit

---
+++ LearnEightyTwenty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnEightyTwenty
+//
+//  Created by Umair Salam on 4/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnEightyTwenty/LearnEightyTwentyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnEightyTwentyApp.swift
+//  LearnEightyTwenty
+//
+//  Created by Umair Salam on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnEightyTwentyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
