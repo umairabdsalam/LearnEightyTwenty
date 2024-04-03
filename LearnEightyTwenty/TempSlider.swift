6f01219f289135794db6cd4076f72133e167aa0f
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-04-03 17:19:00 +0100

    Initial Commit

---
+++ LearnEightyTwenty/TempSlider.swift
@@ -0,0 +1,37 @@
+//
+//  TempSlider.swift
+//  LearnEightyTwenty
+//
+//  Created by Umair Salam on 4/3/24.
+//
+
+import SwiftUI
+
+struct TempSlider: View {
+    
+    @State var showGreeting = true
+    @State var celcius: Double = 0
+    
+    var body: some View {
+        VStack {
+            Toggle(isOn: $showGreeting) {
+                                    Text("Show welcome message")
+                            }.padding()
+
+                            if showGreeting {
+                                  Text("Hello World")
+                         }
+
+        }
+        .padding()
+        
+        VStack {
+            Slider(value: $celcius, in: -100...100)
+              Text("\(celcius) °C is \(celcius * 9 / 5 + 32) °F")
+        }
+    }
+}
+
+#Preview {
+    TempSlider()
+}
